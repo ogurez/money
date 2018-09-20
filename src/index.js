b88574e1c53085f74a764ebf2fe481a935d0e84c
Ray_Bruges <ogurezslava@gmail.com>
2018-09-20 22:59:41 +0300

    first try

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
-    
+   
     let Error={
         error: "You are rich, my friend! We don't have so much coins for exchange"
     }
 
-    let money=[50,25,10,5,1];
+    let money=[1,5,10,25,50];
     let index=money.length-1;
     let splits=[];
     let empty={};
-
+    let exchange={};
+    let colh=1;
+    let colq=1;
+    let cold=1;
+    let coln=1;
+    let colp=1;
     if(currency<=0){
         return empty;
     }
@@ -22,11 +27,28 @@ module.exports = function makeExchange(currency) {
         if(currency>=money[index]){
             currency -=money[index];
             splits.push(money[index]);
+            
+            if(money[index]==50){
+                exchange.H=colh++;
+            }
+            else if(money[index]==25){
+                exchange.Q=colq++;
+            }
+            else if(money[index]==10){
+                exchange.D=cold++;
+            }
+            else if(money[index]==5){
+                exchange.N=coln++;
+            }
+            else if(money[index]==1){
+                exchange.P=colp++;
+            }
+
         }
         else index--;
     }
     /*if(money.indexOf(currency)!==0){
         return money[money.indexOf(currency)];
 }*/
-   return splits;
+   return exchange;
 }
